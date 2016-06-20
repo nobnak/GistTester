406a9e236f29ab7cfa6bf083011bfdeecb7ca25c
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-06-20 15:22:09 +0900

    Camera aspect controller

---
+++ Assets/Packages/Gist/Examples/CameraAspectTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gist {
+        
+    [ExecuteInEditMode]
+    public class CameraAspectTest : MonoBehaviour {
+        public Camera targetCam;
+        public Positioner[] positions;
+
+        void Update() {
+            if (positions == null || targetCam == null)
+                return;
+
+            for (var i = 0; i < positions.Length; i++) {
+                var pos = positions[i];
+                if (pos == null || pos.target == null)
+                    continue;
+
+                var vp = pos.viewportPos;
+                vp.z = vp.z * (targetCam.farClipPlane - targetCam.nearClipPlane) + targetCam.nearClipPlane;
+                pos.target.position = targetCam.ViewportToWorldPoint (vp);
+            }
+        }
+
+        [System.Serializable]
+        public class Positioner {
+            public Transform target;
+            public Vector3 viewportPos;
+        }
+    }
+}
