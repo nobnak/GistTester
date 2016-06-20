68a27922d941c3f85738042348ddd2c943d50d4d
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-06-20 15:40:14 +0900

    no message

--- Assets/Packages/Gist/Examples/CameraAspectTest.cs
+++ Assets/Packages/Gist/Examples/CameraAspectTest.cs
@@ -8,12 +8,25 @@ namespace Gist {
         public Camera targetCam;
         public Positioner[] positions;
 
+        public float mouseSize = 1f;
+        public float mouseDepth = 10f;
+        public Color mouseColor = Color.white;
+
+        GLFigure _fig;
+
+        void Awake() {
+            _fig = new GLFigure ();
+        }
+        void OnDestroy() {
+            if (_fig != null)
+                _fig.Dispose ();
+        }
         void Update() {
-            if (positions == null || targetCam == null)
+            if (positions == null || targetCam == null || _fig == null)
                 return;
 
             for (var i = 0; i < positions.Length; i++) {
-                var pos = positions[i];
+                var pos = positions [i];
                 if (pos == null || pos.target == null)
                     continue;
 
@@ -21,6 +34,15 @@ namespace Gist {
                 vp.z = vp.z * (targetCam.farClipPlane - targetCam.nearClipPlane) + targetCam.nearClipPlane;
                 pos.target.position = targetCam.ViewportToWorldPoint (vp);
             }
+        }
+        void OnRenderObject() {
+            if (_fig == null || targetCam == null)
+                return;
+            
+            var mousePosScreen = Input.mousePosition;
+            mousePosScreen.z = mouseDepth;
+            var mousePos = targetCam.ScreenToWorldPoint (mousePosScreen);
+            _fig.FillCircle (mousePos, targetCam.transform.rotation, mouseSize * Vector2.one, mouseColor);
         }
 
         [System.Serializable]
